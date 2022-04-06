969115016792749b153a579a6bfb0ff8421929a3
tcastron <thomascastronovo@icloud.com>
2022-04-06 13:58:15 +0200

    Removed support for file storage

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-import json
-import os.path
-
+import requests
 from fastapi import FastAPI, Query
 from starlette import status
 
@@ -27,22 +25,7 @@ app = FastAPI(
 )
 
 
-def save_token(username, token):
-    if os.path.exists('data.json') and os.path.getsize('data.json'):
-        with open('data.json', 'r+') as f:
-            data = json.load(f)
-            data[username] = token
-            f.seek(0)
-            json.dump(data, f, indent=4)
-            f.truncate()
-    else:
-        with open('data.json', 'w+') as f:
-            data = {username: token}
-            f.seek(0)
-            json.dump(data, f, indent=4)
-
-
-@app.get("/link", status_code=status.HTTP_200_OK, tags=["Authentication"])
+@app.get("/link", tags=["Authentication"])
 async def authorize_link():
     """Get the authorization link that streamers need to click to connect to our application"""
     link = "https://www.streamlabs.com/api/v1.0/authorize?client_id=" + settings.CLIENT_ID
@@ -58,13 +41,11 @@ async def authorize(code: str = Query(..., description="code given from the auth
     """Callback url for our application to get user token when authorizing connection to our app"""
     access_token, refresh_token = get_token(code)
     user_data = get_user_data(access_token)
-    # username = user_data["streamlabs"]["username"]
-    # save_token(username, access_token)
     create_user(user_data['streamlabs'], access_token, refresh_token)
     return {"message": "Everything went well, thank you for your help"}
 
 
-@app.get("/user", status_code=status.HTTP_200_OK, tags=["User"], response_model=dict)
+@app.get("/user", tags=["User"], response_model=dict)
 async def user_data(username: str = Query(..., description="username of the user")):
     """Get user data from the database"""
     return get_user({'display_name': username})
