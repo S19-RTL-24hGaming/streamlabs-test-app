3a4a953f3c0ad4b1aaa80f81123d8235d10509dd
tcastron <thomascastronovo@icloud.com>
2022-04-11 18:49:05 +0200

    Temporary make code non-async again

--- standalone/streamlabs.py
+++ standalone/streamlabs.py
@@ -2,48 +2,48 @@ from datetime import datetime
 
 import requests
 from fastapi.encoders import jsonable_encoder
-from motor.motor_asyncio import AsyncIOMotorClient
+# from motor.motor_asyncio import AsyncIOMotorClient
 
 from api.settings import settings
 from core.databases.mongo_handler import db
 from core.models.donations import Donation, OutputDonation
 
 
-client = AsyncIOMotorClient(settings.MONGO_URI)
-as_db = client[settings.MONGO_DB]
-async_donations = as_db.test_donations
+# client = AsyncIOMotorClient(settings.MONGO_URI)
+# as_db = client[settings.MONGO_DB]
+# async_donations = as_db.test_donations
 
 donations = db['test_donations']
 
 
-async def create_donation_async(donation: Donation, streamer_id: int, created_at: datetime = None) -> str:
-    """Insert a donation inside of the database
+# async def create_donation_async(donation: Donation, streamer_id: int, created_at: datetime = None) -> str:
+#     """Insert a donation inside of the database
+#
+#     :param donation: donation in the form of a Donation object
+#     :param streamer_id: id of the streamer
+#     :param created_at: datetime of the donation
+#     """
+#     if created_at:
+#         don = OutputDonation(**donation.dict(), streamer_id=streamer_id, created_at=created_at)
+#     else:
+#         don = OutputDonation(**donation.dict(), streamer_id=streamer_id, created_at=datetime.now())
+#     data = jsonable_encoder(don)
+#     _id = async_donations.insert_one(data).inserted_id
+#     return _id
 
-    :param donation: donation in the form of a Donation object
-    :param streamer_id: id of the streamer
-    :param created_at: datetime of the donation
-    """
-    if created_at:
-        don = OutputDonation(**donation.dict(), streamer_id=streamer_id, created_at=created_at)
-    else:
-        don = OutputDonation(**donation.dict(), streamer_id=streamer_id, created_at=datetime.now())
-    data = jsonable_encoder(don)
-    _id = async_donations.insert_one(data).inserted_id
-    return _id
 
-
-async def process_donation_async(result: dict):
-    """Process a donation
-
-    :param dict result: donation dat fron the API
-    """
-    donation_data = result["donation"]
-    streamer_id = int(result["member"]["user"]['id'])
-    message = donation_data["comment"]["text"] if donation_data["comment"] else ""
-    donation = Donation(donation_id=donation_data['id'], amount=float(donation_data['converted_amount']) / 100,
-                        donor=donation_data['display_name'], message=message)
-    await create_donation_async(donation, streamer_id,
-                                datetime.strptime(donation_data['created_at'][:-6], "%Y-%m-%dT%H:%M:%S"))
+# async def process_donation_async(result: dict):
+#     """Process a donation
+#
+#     :param dict result: donation dat fron the API
+#     """
+#     donation_data = result["donation"]
+#     streamer_id = int(result["member"]["user"]['id'])
+#     message = donation_data["comment"]["text"] if donation_data["comment"] else ""
+#     donation = Donation(donation_id=donation_data['id'], amount=float(donation_data['converted_amount']) / 100,
+#                         donor=donation_data['display_name'], message=message)
+#     await create_donation_async(donation, streamer_id,
+#                                 datetime.strptime(donation_data['created_at'][:-6], "%Y-%m-%dT%H:%M:%S"))
 
 
 def create_donation(donation: Donation, streamer_id: int, created_at: datetime = None) -> str:
@@ -70,7 +70,7 @@ def process_donation(result: dict):
     donation_data = result["donation"]
     streamer_id = int(result["member"]["user"]['id'])
     message = donation_data["comment"]["text"] if donation_data["comment"] else ""
-    donation = Donation(donation_id=donation_data['id'], amount=donation_data['converted_amount'],
+    donation = Donation(donation_id=donation_data['id'], amount=float(donation_data['converted_amount']) / 100,
                         donor=donation_data['display_name'], message=message)
     create_donation(donation, streamer_id, datetime.strptime(donation_data['created_at'][:-6], "%Y-%m-%dT%H:%M:%S"))
 
@@ -92,8 +92,8 @@ def get_team_donations(team_id: str, donation_id: str = None):
     data = response.json()
     for result in data:
         try:
-            # process_donation(result)
-            process_donation_async(result)
+            process_donation(result)
+            # process_donation_async(result)
         except Exception as e:
             print(e)
             print(result)
