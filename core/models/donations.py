fe3ab9efebb6cab1decc0774731c698ae5b9f605
tcastron <thomascastronovo@icloud.com>
2022-04-11 18:44:07 +0200

    Adapted donation model to charity API

--- core/models/donations.py
+++ core/models/donations.py
@@ -9,10 +9,10 @@ from core.models.utils import PyObjectId
 class Donation(BaseModel):
     id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
     donation_id: int = Field(..., description="The donation's unique identifier.")
-    amount: float = Field(..., description="The donation's amount.")
+    amount: float = Field(..., description="The donation's amount.", gt=0)
     currency: str = Field("EUR", const=True, description="The donation's currency.")
-    donor: str = Field(..., description="The donation's donor.", min_length=2, max_length=25)
-    message: str = Field(..., description="The donation's message.", max_length=255)
+    donor: str = Field(..., description="The donation's donor.", min_length=1, max_length=140)
+    message: str = Field(..., description="The donation's message.", max_length=500)
 
     class Config:
         allow_population_by_field_name = True
