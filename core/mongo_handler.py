81f6932f26d3771b23e8c1dd700f85bad3160058
dev@dreemcloud.net <fluffy.streamlab.api@gmail.com>
2022-04-15 16:33:00 +0200

    try to make async function

--- core/mongo_handler.py
+++ core/mongo_handler.py
@@ -2,57 +2,65 @@ from datetime import datetime
 from typing import Union
 
 from fastapi.encoders import jsonable_encoder
-from pymongo import MongoClient
+# from pymongo import MongoClient
+
+
+
+# from motor_asyncio import AsyncIOMotorClient
+import asyncio
+from motor.motor_asyncio import AsyncIOMotorClient 
+
+
 
 from api.settings import settings
 from core.models.donations import Donation, OutputDonation
 from core.models.streamers import Streamer, DatabaseStreamer
 
-client = MongoClient(settings.MONGO_URI)
+client = AsyncIOMotorClient(settings.MONGO_URI)
 db = client[settings.MONGO_DB]
 
 donations = db['donations']
 users = db['streamers']
 
-
-def get_donations() -> list[OutputDonation]:
+async def get_donations() -> list[OutputDonation]:
 	"""Get all donations in the database
 
 	:return: list of donationss
 	"""
 	result = []
-	for donation in donations.find({}):
+	for donation in await donations.find({}):
 		result.append(OutputDonation(**donation))
 	return result
 
 
-def get_donation(donation_id) -> Union[OutputDonation, None]:
+async def get_donation(donation_id) -> Union[OutputDonation, None]:
 	"""Get a donation from the database by using its id
 
 	:param int donation_id: id of the streamlab donation
 	:return: donation dict
 	"""
-	if (donation := donations.find_one({'donation_id': donation_id})) is None:
+	donation = await donations.find_one({'donation_id': donation_id})
+	if (donation is None):
 		return None
 	return OutputDonation(**donation)
 
 
-def get_filtered_donations(filters: dict) -> list[OutputDonation]:
+async def get_filtered_donations(filters: dict) -> list[OutputDonation]:
 	"""Get donations in the database by filtering them
 
 	:param filters: dict that contains the mongodb filters
 	:return:
 	"""
 	result = []
-	for donation in donations.find(filters):
+	for donation in await donations.find(filters):
 		result.append(OutputDonation(**donation))
 	return result
 
 
-def get_donations_scoreboard() -> list[Donation]:
+async def get_donations_scoreboard() -> list[Donation]:
 	"""Get the top donations in form of a scoreboard"""
 	result = []
-	for donation in donations.find({}).sort('amount', -1).limit(10):
+	for donation in await donations.find({}).sort('amount', -1).limit(10):
 		result.append(Donation(**donation))
 	return result
 
@@ -69,46 +77,49 @@ async def create_donation(donation: Donation, streamer_id: int, created_at: date
 	else:
 		don = OutputDonation(**donation.dict(), streamer_id=streamer_id, created_at=datetime.now())
 	data = jsonable_encoder(don)
-	test_donation = get_donation(data['donation_id'])
+	test_donation = await get_donation(data['donation_id'])
 	if test_donation == None:
-		return donations.insert_one(data).inserted_id
+		test = await donations.insert_one(data)
+		return test.inserted_id
 	return test_donation.id
 
 
-def get_streamers() -> list[Streamer]:
+async def get_streamers() -> list[Streamer]:
 	"""Get all streamers from the Database
 
 	:return: all streamers in form of dict
 	"""
 	result = []
-	for user in users.find({}):
+	for user in await users.find({}):
 		result.append(Streamer(**user))
 	return result
 
 
-def get_streamer(user_filter: dict) -> Union[Streamer, None]:
+async def get_streamer(user_filter: dict) -> Union[Streamer, None]:
 	"""Get a streamer by its streamlabs id from the database
 
 	:param dict user_filter: filter for the streamer
 	:return: streamer data from the database
 	"""
-	if (user := users.find_one(user_filter)) is None:
+	user = await users.find_one(user_filter)
+	if (user is None):
 		return None
 	return Streamer(**user)
 
 
-def get_streamer_token(username: str) -> str:
+async def get_streamer_token(username: str) -> str:
 	"""Get access_token for streamer
 
 	:param str username: name of the streamer
 	:return: access_token
 	"""
-	if (token := users.find_one({'username': username}, {'_id': 0, 'access_token': 1})) is None:
+	token = await users.find_one({'username': username}, {'_id': 0, 'access_token': 1})
+	if (token is None):
 		return ""
 	return token
 
 
-def create_streamer(user: dict, access_token: str, refresh_token: str, socket_token: str) -> str:
+async def create_streamer(user: dict, access_token: str, refresh_token: str, socket_token: str) -> str:
 	"""Insert a streamer in the database
 
 	:param dict user: user data
@@ -121,7 +132,7 @@ def create_streamer(user: dict, access_token: str, refresh_token: str, socket_to
 			"access_token": access_token, "refresh_token": refresh_token, "socket_token": socket_token}
 	streamer = DatabaseStreamer(**data, created_at=datetime.now(), updated_at=datetime.now())
 	data = jsonable_encoder(streamer)
-	if users.find_one({'user_id': user['id']}):
-		return users.update_one({'user_id': user['id']}, {'$set': data}).upserted_id
-	_id = users.insert_one(data).inserted_id
+	if await users.find_one({'user_id': user['id']}):
+		return await users.update_one({'user_id': user['id']}, {'$set': data}).upserted_id
+	_id = await users.insert_one(data).inserted_id
 	return _id
