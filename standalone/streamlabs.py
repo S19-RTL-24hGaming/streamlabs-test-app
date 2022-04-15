b194716b4dfeccc99e4a7fb262eda3fc4b31caea
dev@dreemcloud.net <fluffy.streamlab.api@gmail.com>
2022-04-15 16:33:20 +0200

    try to make async

--- standalone/streamlabs.py
+++ standalone/streamlabs.py
@@ -9,6 +9,42 @@ from core.mongo_handler import db, create_donation, get_donation
 from core.models.donations import Donation, OutputDonation
 from core.utils.webhooks import send_errorhook
 
+
+
+# client = AsyncIOMotorClient(settings.MONGO_URI)
+# as_db = client[settings.MONGO_DB]
+# async_donations = as_db.test_donations
+# from core.utils.webhooks import send_errorhook
+
+# async def create_donation_async(donation: Donation, streamer_id: int, created_at: datetime = None) -> str:
+#     """Insert a donation inside of the database
+#
+#     :param donation: donation in the form of a Donation object
+#     :param streamer_id: id of the streamer
+#     :param created_at: datetime of the donation
+#     """
+#     if created_at:
+#         don = OutputDonation(**donation.dict(), streamer_id=streamer_id, created_at=created_at)
+#     else:
+#         don = OutputDonation(**donation.dict(), streamer_id=streamer_id, created_at=datetime.now())
+#     data = jsonable_encoder(don)
+#     _id = async_donations.insert_one(data).inserted_id
+#     return _id
+
+
+# async def process_donation_async(result: dict):
+#     """Process a donation
+#
+#     :param dict result: donation dat fron the API
+#     """
+#     donation_data = result["donation"]
+#     streamer_id = int(result["member"]["user"]['id'])
+#     message = donation_data["comment"]["text"] if donation_data["comment"] else ""
+#     donation = Donation(donation_id=donation_data['id'], amount=float(donation_data['converted_amount']) / 100,
+#                         donor=donation_data['display_name'], message=message)
+#     await create_donation_async(donation, streamer_id,
+#                                 datetime.strptime(donation_data['created_at'][:-6], "%Y-%m-%dT%H:%M:%S"))
+
 async def process_donation(result: dict):
 	"""Process a donation
 
@@ -50,14 +86,14 @@ async def get_team_donations(team_id: str, donation_id: str = None):
 
 async def start():
 	id = await get_team_donations("337671010314752000")
-	donation_id = get_donation(id)
+	donation_id = await get_donation(id)
 	print(id)
 	print(donation_id)
 	print('exit') ##TODO REMOVE
 
 if __name__ == '__main__':
-	asyncio.run(start())
 
+	asyncio.get_event_loop().run_until_complete(start())
 	# while (id != None)
 	# 	id = get_team_donations("337671010314752000", id)
 	# 	donation_id = get_donation(id)
